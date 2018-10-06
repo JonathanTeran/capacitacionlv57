a1ab7b99608df57b0d10da759bd07ceebeed943d
ernesto.liberio@gmail.com <ernesto.liberio@gmail.com>
2018-10-06 16:48:15 -0500

    avance

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-12">
           
             <div class="card">
             <div class="card-header">
